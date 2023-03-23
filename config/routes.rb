60c2ed019bdc9fc59429110b0b1a4a3409201956
03040610 <yukikobayashi0304@gmail.com>
2023-03-23 21:05:28 +0900

    ヘッダーの部分テンプレートのhtmlのデモ版

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :teachers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "teacher#index"
+  resources :teachers do
+    collection do
+      get 'search'
+    end
+  end
 end
