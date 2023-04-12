1198d84e2fe9a135c144d2a72ba82b33e1bd75f3
03040610 <yukikobayashi0304@gmail.com>
2023-04-12 11:45:50 +0900

    エラー修正

--- app/controllers/rooms_controller.rb
+++
@@ -1,8 +0,0 @@
-class RoomsController < ApplicationController
-
-  def new
-    @rooms = Room.all
-    @teacher = Teacher.all
-  end
-  
-end
--- db/schema.rb
+++ db/schema.rb
@@ -33,13 +33,6 @@ ActiveRecord::Schema.define(version: 2023_03_31_035803) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "teachers_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["teachers_id"], name: "index_rooms_on_teachers_id"
-  end
-
   create_table "teachers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: ""
     t.string "encrypted_password", default: ""
@@ -70,5 +63,5 @@ ActiveRecord::Schema.define(version: 2023_03_31_035803) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "rooms", "teachers", column: "teachers_id"
+  add_foreign_key "teachers", column: "teachers_id"
 end
